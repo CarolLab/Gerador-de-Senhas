85b1fec4154f35f07d382a721b261d94e9b9c9a5
CarolLab <bessascarolina@gmail.com>
2025-07-04 20:53:46 +0100

    Nova entry de nº de letras,melhorias incluídas

--- Gerador_de_senhas.py
+++ Gerador_de_senhas.py
@@ -3,29 +3,47 @@ import random
 
 
 def funcao():
+    try:
+        qt = int(size.get()) #get as a int, the valor of the entry SIZE
 
-    sim = "aAbcCdDeEfghHijklmnNpPqrsStTuvwxyz123456789#$%*@"
-    senha = ""
 
-    for _ in range(5):
-        senha += random.choice(sim)
+        sim = "aAbcCdDeEfghHijklmnNpPqrsStTuvwxyz123456789#$%*@"
+        senha = ""
 
-    result = Label(jan, text=senha, pady = 10, padx = 10)
-    result.grid(row=2, column=0)
+        for _ in range(qt):
+            senha += random.choice(sim)
+
+        senha_label.config(text = senha, fg = "Black", font = "Roboto 10 bold")
+
+    except:
+        senha_label.config(text="Error", fg = "red3")
+
 
 
 
 jan = Tk()
 
 jan.title("Gerador de senhas")
-jan.geometry("250x100")
+jan.geometry("250x150")
 
 
+#Entrys
+size = Entry(jan, cursor = "hand2")
+size.grid(row = 1, column = 0, pady = 10)
+
+
+#Labels
 titl = Label(jan, text = "Gerador De Senhas", padx=10, pady=5, font = "Rubix 19")
 titl.grid(row = 0, column = 0)
 
 
-gerar = Button(jan, text = "Gerar", command= funcao, padx=10)
-gerar.grid(row = 1, column = 0)
+
+senha_label = Label(jan, text = "", pady= 8) #Label da senha
+senha_label.grid(row = 3, column = 0)
+
+#Buttons
+gerar = Button(jan, text = "Gerar", padx=10,
+               command= funcao) #Botão de gerar a senha
+gerar.grid(row = 2, column = 0)
 
 jan.mainloop()
