1895587827ab6ae9bc372b8b75830a0d9bd5be7f
CarolLab <bessascarolina@gmail.com>
2025-08-02 23:19:53 +0100

    Alterações: Nome de variáveis modificadas, novo label

--- Gerador_de_senhas.py
+++ Gerador_de_senhas.py
@@ -21,12 +21,13 @@ def gerar_senha():
         senha_label.config(text="Error", fg = "red3")
 
 
+
 #################################################################
 
 jan = tk.Tk()
 
 jan.title("Gerador de senhas")
-jan.geometry("250x160")
+jan.geometry("270x180")
 jan.minsize(245,150)
 
 #Configuração das colunas e linhas
@@ -37,22 +38,24 @@ jan.columnconfigure(0, weight = 1)
 jan.rowconfigure([0,1,2], weight = 1)
 
 #Entrys
+#Label informativa
+tk.Label(jan, text = "Insira o tamanho da senha").grid(row = 1, column = 0, pady = 10)
 size = tk.Entry(jan) #size of the password
-size.grid(row = 1, column = 0, pady = 10)
+size.grid(row = 2, column = 0, pady = 10)
 
 
 #Labels
-titl = tk.Label(jan, text = "Gerador De Senhas", padx=10, pady=5, font = "Rubix 19")
+titulo = tk.Label(jan, text = "Gerador De Senhas", padx=10, pady=5, font = "Rubix 19")
 senha_label = tk.Label(jan, text = "") #Label da senha
 
 
-titl.grid(row = 0, column = 0)
-senha_label.grid(row = 3, column = 0, pady = 10)
+titulo.grid(row = 0, column = 0)
+senha_label.grid(row = 4, column = 0, pady = 10)
 
 
 #Buttons
-gerar = tk.Button(jan, text = "Gerar", padx=10, cursor = "hand2", bg = "#32a858",
+butao_gerar = tk.Button(jan, text = "Gerar", padx=10, cursor = "hand2", bg = "#32a858",
                command= gerar_senha) #Botão de gerar a senha
-gerar.grid(row = 2, column = 0)
+butao_gerar.grid(row = 3, column = 0)
 
 jan.mainloop()
