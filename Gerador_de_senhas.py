06dd093c44473f32a62a111e62d7b035d49fd8a6
CarolLab <bessascarolina@gmail.com>
2025-08-03 22:29:45 +0100

    Implementação de frames,melhorias no gestor de geometria

--- Gerador_de_senhas.py
+++ Gerador_de_senhas.py
@@ -4,8 +4,8 @@ import random
 
 def gerar_senha():
     try:
-        if 0 < int(size.get()) < 31:
-            qt = int(size.get()) #get as a int, the valor of the entry SIZE
+        if 0 < int(pass_size.get()) < 31:
+            qt = int(pass_size.get()) #get as a int, the valor of the entry SIZE
 
             abc = "aAbcCdDeEfghHijklmnNpPqrsStTuvwxyz123456789#$%*@"
             senha = ""
@@ -27,35 +27,52 @@ def gerar_senha():
 jan = tk.Tk()
 
 jan.title("Gerador de senhas")
-jan.geometry("270x180")
-jan.minsize(270,180)
+jan.geometry("355x255")
+jan.minsize(355,255)
 
-#Configuração das colunas e linhas
-#Colunas
+#Configuração de linhas e colunas da janela
+jan.rowconfigure(0, weight = 3)
+jan.rowconfigure(1, weight = 24)
+
 jan.columnconfigure(0, weight = 1)
 
-#Linhas
-jan.rowconfigure([0,1,2], weight = 1)
 
-#Entrys
+#Frames- - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+frame_1 = tk.Frame(jan,bg = "DarkOliveGreen3")
+frame_2 = tk.Frame(jan)
 
-tk.Label(jan, text = "Insira o tamanho da senha", font = ("Garamond", 10, "bold")).grid(row = 1, column = 0, pady = 10)#Label informativo
-size = tk.Entry(jan) #size of the password
-size.grid(row = 2, column = 0, pady = 10, ipady = 3)
+#Configuração dos frames
+#Frame1
+frame_1.rowconfigure(0, weight = 1)
+frame_1.columnconfigure(0, weight = 1)
+
+#Frame2
+frame_2.columnconfigure(0, weight = 1)
+
+#Exibição dos frames
+frame_1.grid(row = 0, column = 0, sticky = "nswe")
+frame_2.grid(pady = 10,row = 1, column = 0, sticky = "nswe")
 
 
-#Labels
-titulo = tk.Label(jan, text = "Gerador De Senhas", padx=10, pady=5, font = "Rubix 19")
-senha_label = tk.Label(jan, text = "") #Label da senha
+#Entrys - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+tk.Label(frame_2, text = "Insira um tamanho para a senha",
+         font = ("Garamond", 10, "bold")).grid(row = 1, column = 0, pady = 10)#Label informativo
+pass_size = tk.Entry(frame_2) #Tamanho a senha
+
+pass_size.grid(row = 2, column = 0, pady = 8, ipady = 3)
 
 
-titulo.grid(row = 0, column = 0)
+#Labels- - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+titulo = tk.Label(frame_1, text = "Gerador De Senhas",font = ("Rubix", 16, "bold"),
+                  bg = "DarkOliveGreen3")#Título
+senha_label = tk.Label(frame_2, text = "") #Label da senha
+
+titulo.grid(pady = 8,row = 0, column = 0,sticky = "nswe")
 senha_label.grid(row = 4, column = 0, pady = 10)
 
-
-#Buttons
-butao_gerar = tk.Button(jan, text = "Gerar", padx=10, cursor = "hand2", bg = "#32a858",
+#Buttons- - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+butao_gerar = tk.Button(frame_2, text = "Gerar", padx=10, cursor = "hand2", bg = "#32a858",
                command= gerar_senha) #Botão de gerar a senha
-butao_gerar.grid(row = 3, column = 0)
+butao_gerar.grid(pady = 8,row = 3, column = 0)
 
 jan.mainloop()
